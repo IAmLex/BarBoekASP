5d9c8265fcb0d7b965c1b7f62f56aa96e077d727
IAmLex <lexkrooswijk@live.nl>
2020-05-20 21:02:41 +0200

    Added remove all users feature to import
    + Front end validation for File upload

--- BarBoekASP/Models/ImportUsersModel.cs
+++ BarBoekASP/Models/ImportUsersModel.cs
@@ -1,11 +1,14 @@
-using System;
 using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace BarBoekASP.Models
 {
     public class ImportUsersModel
     {
+        [Required(ErrorMessage = "Selecteer een bestand.")]
         public IFormFile File { get; set; }
+
         public bool RemoveCurrent { get; set; }
     }
 }
