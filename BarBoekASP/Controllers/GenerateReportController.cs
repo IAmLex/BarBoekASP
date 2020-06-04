2400d90d173244e8f673ebf71377d343a4b9a995
S-Vermeer <S.Vermeer@fhict.nl>
2020-06-04 21:03:08 +0200

    Added Radiobutton + memberList
    Added radiobuttons and memberslist to generate report view/controller + edited css file to make it look better.
    
    Note: backgroundcolor blue is so testing is better, will be removed later

--- BarBoekASP/Controllers/GenerateReportController.cs
+++ BarBoekASP/Controllers/GenerateReportController.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BarBoekASP.Data.MySQL;
+using BarBoekASP.Data.Repositories;
+using BarBoekASP.Interfaces;
+using BarBoekASP.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace BarBoekASP.Controllers
 {
     public class GenerateReportController : Controller
     {
+        iMemberRetrieveContext _iMemberRetrieveContext;
+        MemberRetRepository memberRetRepository;
+        public GenerateReportController(IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            _iMemberRetrieveContext = new MemberMySQLContext(connectionString);
+            memberRetRepository = new MemberRetRepository(_iMemberRetrieveContext);
+        }
         public IActionResult Index()
         {
+            List<MemberDTO> members = new List<MemberDTO> { };
+            members = memberRetRepository.GetAll();
+            ViewData["MemberList"] = members;
             return View();
         }
     }
