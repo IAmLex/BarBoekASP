51c7520c29e4a65f9f6f7339aaeb1edacfc9e1c2
Sicong Nie <niesicong1999@gmail.com>
2020-06-04 17:55:33 +0200

    Registreren toevoegen

---
+++ BarBoekASP/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BarBoekASP.Interfaces;
+using BarBoekASP.Logic.Club;
+using BarBoekASP.Logic.UserLogin;
+using BarBoekASP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarBoekASP.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login(ClubModel club)
+        {
+            iClubRetrieveContext context = HttpContext.RequestServices.GetService(typeof(BarBoekASP.Data.MySQL.ClubMySQLContext)) as iClubRetrieveContext;
+            iUserRetrieveContext ucontext = HttpContext.RequestServices.GetService(typeof(BarBoekASP.Data.MySQL.UserMySQLContext)) as iUserRetrieveContext;
+            UserTest user = new UserTest();
+            user.UEmail = club.ClubNumber;
+            user.Password = club.Postcode;
+            bool check = context.Inloggen(club);
+            bool ucheck = ucontext.Inloggen(user);
+            if (check)
+            {
+                return View("Confirm");
+            }
+            else
+            {
+                if (ucheck)
+                {
+                    return View("Fout");
+                }
+                else
+                {
+                    ModelState.AddModelError("Postcode", "Password incorrect.");
+                    return View("Index");
+                }
+            }
+        }
+        [HttpGet]
+        public IActionResult Confirm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Confirm(UserTest user)
+        {
+            iUserSaveContext context = HttpContext.RequestServices.GetService(typeof(BarBoekASP.Data.MySQL.UserMySQLContext)) as iUserSaveContext;
+            iUserRetrieveContext retcontext = HttpContext.RequestServices.GetService(typeof(BarBoekASP.Data.MySQL.UserMySQLContext)) as iUserRetrieveContext;
+            if (retcontext.CheckValidate(user) == false)
+            {
+                context.InsertUser(user);
+                return View("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("Email", "User with this email already exists");
+                return View("Confirm");
+            }
+
+        }
+    }
+}
---
+++ BarBoekASP/Data/MySQL/UserMySQLContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using BarBoekASP.Logic.UserLogin;
+using BarBoekASP.Interfaces;
+
+namespace BarBoekASP.Data.MySQL
+{
+    public class UserMySQLContext:BaseMySQLContext,iUserRetrieveContext,iUserSaveContext
+    {
+        public UserMySQLContext(string connString) : base(connString)
+        {
+
+        }
+        public List<UserTest> GetAllUsers()
+        {
+            string query = "Select * from user ";
+            List<KeyValuePair<string, string>> parameters = new List<KeyValuePair<string, string>>();
+            DataSet results = ExecuteQuery(query, parameters);
+
+            List<UserTest> users = new List<UserTest>();
+
+            if (results != null)
+            {
+
+                for (int x = 0; x < results.Tables[0].Rows.Count; x++)
+                {
+                    UserTest user = DataSetParser.DataSetToUser(results, x);
+                    users.Add(user);
+                }
+            }
+            return users;
+        }
+        public void InsertUser(UserTest user)
+        {
+            string sql = " INSERT INTO user(email, password) VALUES(@email,@password);";
+            List<KeyValuePair<string, string>> parameters = new List<KeyValuePair<string, string>>();
+            parameters.Add(new KeyValuePair<string, string>("email", user.UEmail));
+            parameters.Add(new KeyValuePair<string, string>("password", user.Password));
+            DataSet result = ExecuteQuery(sql, parameters);
+        }
+        public bool Inloggen(UserTest user)
+        {
+            bool check = false;
+            string mysql = "Select * from user where email=@email and password=@password";
+            List<KeyValuePair<string, string>> parameters = new List<KeyValuePair<string, string>>();
+            parameters.Add(new KeyValuePair<string, string>("email", user.UEmail));
+            parameters.Add(new KeyValuePair<string, string>("password", user.Password));
+            DataSet results = ExecuteQuery(mysql, parameters);
+            if (results != null && results.Tables[0].Rows.Count > 0)
+            {
+                check = true;
+            }
+            return check;
+        }
+        public bool CheckValidate(UserTest user)
+        {
+            bool check = false;
+            string mysql = "Select * from user where email=@email";
+            List<KeyValuePair<string, string>> parameters = new List<KeyValuePair<string, string>>();
+            parameters.Add(new KeyValuePair<string, string>("email", user.UEmail));
+            DataSet results = ExecuteQuery(mysql, parameters);
+            if (results != null && results.Tables[0].Rows.Count > 0)
+            {
+                check = true;
+            }
+            return check;
+        }
+    }
+}
