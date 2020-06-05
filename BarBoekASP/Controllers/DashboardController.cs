54197efc624ecbd1129c668d03eb40a1939931e0
IAmLex <lexkrooswijk@live.nl>
2020-06-05 13:48:21 +0200

    Added dashboard template

---
+++ BarBoekASP/Controllers/DashboardController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BarBoekASP.Interfaces;
+using BarBoekASP.Logic.Club;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarBoekASP.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
